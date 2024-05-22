aba0d60684fcbf26dcb9950c7bcfc81a2407d0e3
Evelina <evelinalg79@gmail.com>
2024-05-22 22:16:17 +0300

    add group project SlimMom

---
+++ src/redux/auth/authSelectors.js
@@ -0,0 +1,6 @@
+const sid = state => state.auth.sid;
+const getToken = state => state.auth.accessToken;
+//const getRefreshToken = state => state.auth.refreshToken;
+
+const selectors = { sid, getToken };
+export default selectors;
